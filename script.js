dc32bf1ca0fed752544bad6844020e205ba1a555
Mitchell <Hmitchell.hall@gmail.com>
2021-08-03 15:01:01 -0500

    more content and mouseover event added for video

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("campingVid").addEventListener("mouseover", function() {
+	this.play();
+});
+document.getElementById("campingVid").addEventListener("mouseleave", function() {
+	this.pause();
+});
+document.getElementById("hikingVid").addEventListener("mouseover", function() {
+	this.play();
+});
+document.getElementById("hikingVid").addEventListener("mouseleave", function() {
+	this.pause();
+});
